70f1805bea219c771abb24f6d1a65695995f809a
Yurij Mikhalevich <0@39.yt>
2014-07-17 18:10:18 +0400

    Updated tests

--- test/winston-mongodb-test.js
+++ test/winston-mongodb-test.js
@@ -1,16 +1,13 @@
 /*
- * mongodb-test.js: Tests for instances of the MongoDB transport
+ * winston-mongodb-test.js: Tests for instances of the MongoDB transport
  *
- * (C) 2011 Charlie Robbins, Kendrick Taylor
+ * (C) 2011 Charlie Robbins, Kendrick Taylor, Yurij Mikhalevich
  * MIT LICENSE
  *
  */
 
-var path = require('path'),
-    vows = require('vows'),
-    assert = require('assert'),
+var vows = require('vows'),
     winston = require('winston'),
-    helpers = require('winston/test/helpers'),
     transport = require('winston/test/transports/transport'),
     MongoDB = require('../lib/winston-mongodb').MongoDB;
 
@@ -20,25 +17,3 @@ vows.describe('winston-mongodb').addBatch({
     keepAlive: 1000
   })
 }).export(module);
-
-
-//vows.describe('winston-mongodb').addBatch({
-//  'Test MongoDB with replica set': transport(MongoDB, {
-//    db: 'winston',
-//    keepAlive: 1000,
-//    replSet: 'rs0',
-//    hosts: [
-//      {port: 27018},
-//      {port: 27019},
-//      {port: 27020}
-//    ]
-//  })
-//}).export(module);
-
-
-//vows.describe('winston-mongodb').addBatch({
-//  'Test MongoDB with replica set': transport(MongoDB, {
-//    keepAlive: 1000,
-//    dbUri: 'mongodb://localhost:27018,localhost:27019,localhost:27020/winston?replicaSet=rs0'
-//  })
-//}).export(module);
