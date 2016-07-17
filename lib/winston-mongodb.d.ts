d3502c2dfcadab3622ddfccc854402f68e0ba9f5
miton18 <rcd18@hotmail.fr>
2016-07-17 11:19:15 +0200

    typescript definition

---
+++ lib/winston-mongodb.d.ts
@@ -0,0 +1,11 @@
+// Type definitions for winston-mongodb
+// Project: https://github.com/winstonjs/winston-mongodb
+// Definitions by: miton18 <https://github.com/miton18>
+
+///******************
+///  Winston-mongodb v1.6.0
+///******************
+
+declare module "winston-mongodb" {
+    export function MongoDB(options: Object);
+}
