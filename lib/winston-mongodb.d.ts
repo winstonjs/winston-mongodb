77f752d3a1b6b5ebe6c8dc7ca604878c09c3e0a1
Yurij Mikhalevich <0@39.yt>
2016-10-26 20:24:54 +0300

    style fixes

--- lib/winston-mongodb.d.ts
+++ lib/winston-mongodb.d.ts
@@ -2,10 +2,6 @@
 // Project: https://github.com/winstonjs/winston-mongodb
 // Definitions by: miton18 <https://github.com/miton18>
 
-///******************
-///  Winston-mongodb v1.6.0
-///******************
-
-declare module "winston-mongodb" {
+declare module 'winston-mongodb' {
     export function MongoDB(options: Object);
 }
