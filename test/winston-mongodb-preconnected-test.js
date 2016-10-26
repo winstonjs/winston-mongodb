e720c75ccc08cc79b66a91bb0808eaadf15f36ff
Yurij Mikhalevich <0@39.yt>
2016-10-26 21:34:15 +0300

    winston-mongodb-preconnected-test.js: es5->es6; better exporting

--- test/winston-mongodb-preconnected-test.js
+++ test/winston-mongodb-preconnected-test.js
@@ -1,27 +1,26 @@
 /**
- * @module winston-mongodb-preconnected-test
+ * @module 'winston-mongodb-preconnected-test'
  * @fileoverview Tests for instances of the MongoDB transport
  * @license MIT
  * @author 0@39.yt (Yurij Mikhalevich)
  */
+'use strict';
+const vows = require('vows');
+const mongodb = require('mongodb');
+const transport = require('winston/test/transports/transport');
+const MongoDB = require('../lib/winston-mongodb').MongoDB;
 
-var vows = require('vows');
-var mongodb = require('mongodb');
-var transport = require('winston/test/transports/transport');
-var MongoDB = require('../lib/winston-mongodb').MongoDB;
-
-
-mongodb.MongoClient.connect('mongodb://localhost/winston', function(err, db) {
-  if (err) {
-    console.error('winston-mongodb-preconnected-test: error initialising db',
-        err);
-    return;
+vows.describe('winston-mongodb').addBatch({
+  'And instance of the MongoDB Transport with preconnected db object': {
+    topic: function(){
+      mongodb.MongoClient.connect('mongodb://localhost/winston', this.callback);
+    },
+    prepare: function(db){
+      Object.assign(this.context.tests.test, transport(MongoDB, {db}));
+    },
+    test: {
+      dummy: ()=>{}, // hack to be able to execute asynchronously added tests
+    },
   }
-  vows.describe('winston-mongodb').addBatch({
-    'And instance of the MongoDB Transport with preconnected db object':
-        transport(MongoDB, {
-          db: db
-        })
-  }).run();
-});
+}).export(module);
 
