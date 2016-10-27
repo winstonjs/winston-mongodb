ea0bf80c6d72db4f4c65dc4845f5cde56d6e2e27
Yurij Mikhalevich <0@39.yt>
2016-10-28 00:17:27 +0300

    winston-mongodb-test.js: es5->es6; tests united

--- test/winston-mongodb-test.js
+++ test/winston-mongodb-test.js
@@ -1,39 +1,38 @@
 /**
- * @module winston-mongodb-test
+ * @module 'winston-mongodb-test'
  * @fileoverview Tests for instances of the MongoDB transport
  * @license MIT
  * @author charlie@nodejitsu.com (Charlie Robbins)
  * @author 0@39.yt (Yurij Mikhalevich)
  */
-
-var vows = require('vows');
-var mongodb = require('mongodb');
-var Promise = require('bluebird');
-var transport = require('winston/test/transports/transport');
-var MongoDB = require('../lib/winston-mongodb').MongoDB;
+'use strict';
+const vows = require('vows');
+const mongodb = require('mongodb');
+const transport = require('winston/test/transports/transport');
+const MongoDB = require('../lib/winston-mongodb').MongoDB;
 
 vows.describe('winston-mongodb').addBatch({
-  'An instance of the MongoDB Transport': transport(MongoDB, {
-    db: 'mongodb://localhost/winston'
-  }),
-  'An instance of the MongoDB Transport on capped collection':
-      transport(MongoDB, {
-        db: 'mongodb://localhost/winston',
-        capped: true,
-        collection: 'cappedLog'
-      })
+  '{db: url}': transport(MongoDB, {db: 'mongodb://localhost/winston'}),
 }).addBatch({
-  'An instance of the MongoDB Transport with promise':
-      transport(MongoDB, {
-        db: new Promise(function(resolve, reject) {
-          mongodb.MongoClient.connect('mongodb://localhost/winston',
-              function(err, db) {
-                if (err) {
-                  reject(err);
-                } else {
-                  resolve(db);
-                }
-              });
-        })
-      })
+  '{db: url} on capped collection': transport(MongoDB, {
+    db: 'mongodb://localhost/winston',
+    capped: true,
+    collection: 'cappedLog'
+  }),
+}).addBatch({
+  '{db: promise}': transport(MongoDB, {
+    db: mongodb.MongoClient.connect('mongodb://localhost/winston')
+  })
+}).addBatch({
+  '{db: preconnected}': {
+    topic: function(){
+      mongodb.MongoClient.connect('mongodb://localhost/winston', this.callback);
+    },
+    prepare: function(db){
+      Object.assign(this.context.tests.test, transport(MongoDB, {db}));
+    },
+    test: {
+      dummy: ()=>{}, // hack to be able to execute asynchronously added tests
+    },
+  }
 }).export(module);
