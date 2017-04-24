899d9df68388f6f6fa7b7146f943806d91eb258c
Cyril Su <god@liril.net>
2017-04-24 18:40:21 +0800

    fix: Support noImplicitAny

--- lib/winston-mongodb.d.ts
+++ lib/winston-mongodb.d.ts
@@ -3,5 +3,5 @@
 // Definitions by: miton18 <https://github.com/miton18>
 
 declare module 'winston-mongodb' {
-    export function MongoDB(options: Object);
+    export function MongoDB(options: Object): any;
 }
