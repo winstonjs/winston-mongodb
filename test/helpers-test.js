15310eaad48337f2d931c040b1bde4eb5a0bd26f
Yurij Mikhalevich <yurij@mikhalevi.ch>
2017-12-09 12:46:33 +0300

    initial winston 3.0.0-rc1 support

--- test/helpers-test.js
+++ test/helpers-test.js
@@ -5,7 +5,6 @@
  * @author 0@39.yt (Yurij Mikhalevich)
  */
 'use strict';
-const vows = require('vows');
 const assert = require('assert');
 const helpers = require('../lib/helpers');
 
@@ -22,27 +21,27 @@ const originalData = {
   customError: new CustomError()
 };
 
-vows.describe('winston-mongodb-helpers').addBatch({
-  'prepareMetaData': {
-    topic: helpers.prepareMetaData(originalData),
-    'should preserve Date instances': function(preparedData) {
-      assert.isTrue(preparedData.customDate instanceof Date);
+describe('winston-mongodb-helpers', function() {
+  describe('#prepareMetaData()', function() {
+    let preparedData = helpers.prepareMetaData(originalData);
+    it('should preserve Date instances', function() {
+      assert(preparedData.customDate instanceof Date);
       assert.equal(+preparedData.customDate, +originalData.customDate);
-    },
-    'should store Error objects': function(preparedData) {
-      assert.isTrue(preparedData.standardError instanceof Object);
-      assert.isFalse(preparedData.standardError instanceof Error);
+    });
+    it('should store Error objects', function() {
+      assert(preparedData.standardError instanceof Object);
+      assert(!(preparedData.standardError instanceof Error));
       assert.equal(preparedData.standardError.message, originalData.standardError.message);
       assert.equal(preparedData.standardError.name, originalData.standardError.name);
       assert.equal(preparedData.standardError.stack, originalData.standardError.stack);
-    },
-    'should store extra fields for custom Error objects': function(preparedData) {
-      assert.isTrue(preparedData.customError instanceof Object);
-      assert.isFalse(preparedData.customError instanceof Error);
+    });
+    it('should store extra fields for custom Error objects', function() {
+      assert(preparedData.customError instanceof Object);
+      assert(!(preparedData.customError instanceof Error));
       assert.equal(preparedData.customError.message, originalData.customError.message);
       assert.equal(preparedData.customError.name, originalData.customError.name);
       assert.equal(preparedData.customError.stack, originalData.customError.stack);
       assert.equal(preparedData.customError.testField, originalData.customError.testField);
-    },
-  }
-}).export(module);
+    });
+  });
+});
--- test/winston-mongodb-test.js
+++ test/winston-mongodb-test.js
@@ -6,29 +6,14 @@
  * @author 0@39.yt (Yurij Mikhalevich)
  */
 'use strict';
-const vows = require('vows');
 const mongodb = require('mongodb');
-const transport = require('winston/test/transports/transport');
+const test_suite = require('abstract-winston-transport');
 const MongoDB = require('../lib/winston-mongodb').MongoDB;
 const dbUrl = process.env.USER_WINSTON_MONGODB_URL
     ||process.env.WINSTON_MONGODB_URL||'mongodb://localhost/winston';
 
-vows.describe('winston-mongodb').addBatch({
-  '{db: url}': transport(MongoDB, {db: dbUrl}),
-}).addBatch({'{db: url} on capped collection': transport(MongoDB,
-    {db: dbUrl, capped: true, collection: 'cappedLog'}),
-}).addBatch({
-  '{db: promise}': transport(MongoDB, {db: mongodb.MongoClient.connect(dbUrl)})
-}).addBatch({
-  '{db: preconnected}': {
-    topic: function(){
-      mongodb.MongoClient.connect(dbUrl, this.callback);
-    },
-    prepare: function(db){
-      Object.assign(this.context.tests.test, transport(MongoDB, {db}));
-    },
-    test: {
-      dummy: ()=>{}, // hack to be able to execute asynchronously added tests
-    },
-  }
-}).export(module);
+test_suite({name: '{db: url}', Transport: MongoDB, construct: {db: dbUrl}});
+test_suite({name: '{db: url} on capped collection', Transport: MongoDB,
+    construct: {db: dbUrl, capped: true, collection: 'cappedLog'}});
+test_suite({name: '{db: promise}', Transport: MongoDB,
+    construct: {db: mongodb.MongoClient.connect(dbUrl)}});
