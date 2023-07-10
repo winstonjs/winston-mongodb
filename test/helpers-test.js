68aa9288deb1de0b7033d3b2bd1547fe6172a049
DABH <DABH@users.noreply.github.com>
2023-07-10 01:31:31 -0500

    Lint fixes

--- test/helpers-test.js
+++ test/helpers-test.js
@@ -21,21 +21,21 @@ const originalData = {
   customError: new CustomError()
 };
 
-describe('winston-mongodb-helpers', function() {
-  describe('#prepareMetaData()', function() {
-    let preparedData = helpers.prepareMetaData(originalData);
-    it('should preserve Date instances', function() {
+describe('winston-mongodb-helpers', function () {
+  describe('#prepareMetaData()', function () {
+    const preparedData = helpers.prepareMetaData(originalData);
+    it('should preserve Date instances', function () {
       assert(preparedData.customDate instanceof Date);
       assert.strictEqual(+preparedData.customDate, +originalData.customDate);
     });
-    it('should store Error objects', function() {
+    it('should store Error objects', function () {
       assert(preparedData.standardError instanceof Object);
       assert(!(preparedData.standardError instanceof Error));
       assert.strictEqual(preparedData.standardError.message, originalData.standardError.message);
       assert.strictEqual(preparedData.standardError.name, originalData.standardError.name);
       assert.strictEqual(preparedData.standardError.stack, originalData.standardError.stack);
     });
-    it('should store extra fields for custom Error objects', function() {
+    it('should store extra fields for custom Error objects', function () {
       assert(preparedData.customError instanceof Object);
       assert(!(preparedData.customError instanceof Error));
       assert.strictEqual(preparedData.customError.message, originalData.customError.message);
--- test/winston-mongodb-test.js
+++ test/winston-mongodb-test.js
@@ -14,34 +14,34 @@ const test_suite = require('abstract-winston-transport');
 const MongoDB = require('../lib/winston-mongodb').MongoDB;
 
 const dbUrl = process.env.USER_WINSTON_MONGODB_URL
-    ||process.env.WINSTON_MONGODB_URL||'mongodb://localhost:27017/winston';
+    || process.env.WINSTON_MONGODB_URL || 'mongodb://localhost:27017/winston';
 const dbName = process.env.WINSTON_MONGODB_DBNAME
-    ||'otherWinston';
+    || 'otherWinston';
 
-mongoose.connect(dbUrl, {useNewUrlParser: true});
+mongoose.connect(dbUrl, { useNewUrlParser: true });
 
-describe('winston-mongodb-manual-tests', function() {
-  describe('winston-mongodb', function() {
-    let transport = new MongoDB({db: dbUrl});
-    it('should be closeable', function() {
+describe('winston-mongodb-manual-tests', function () {
+  describe('winston-mongodb', function () {
+    const transport = new MongoDB({ db: dbUrl });
+    it('should be closeable', function () {
       transport.close();
     });
   });
 });
 
-test_suite({name: '{db: url}', Transport: MongoDB, construct: {db: dbUrl}});
-test_suite({name: '{db: url, dbName: string}', Transport: MongoDB,
-    construct: {db: dbUrl, dbName}});
-test_suite({name: '{db: url} on capped collection', Transport: MongoDB,
-    construct: {db: dbUrl, capped: true, collection: 'cappedLog'}});
-test_suite({name: '{db: url, dbName: string} on capped collection', Transport: MongoDB,
-    construct: {db: dbUrl, dbName, capped: true, collection: 'cappedLog'}});
-test_suite({name: '{db: client promise}', Transport: MongoDB,
-    construct: {db: mongodb.MongoClient.connect(dbUrl, {useNewUrlParser: true})}});
-test_suite({name: '{db: client promise, dbName: string}', Transport: MongoDB,
+test_suite({ name: '{db: url}', Transport: MongoDB, construct: { db: dbUrl }});
+test_suite({ name: '{db: url, dbName: string}', Transport: MongoDB,
+  construct: { db: dbUrl, dbName }});
+test_suite({ name: '{db: url} on capped collection', Transport: MongoDB,
+  construct: { db: dbUrl, capped: true, collection: 'cappedLog' }});
+test_suite({ name: '{db: url, dbName: string} on capped collection', Transport: MongoDB,
+  construct: { db: dbUrl, dbName, capped: true, collection: 'cappedLog' }});
+test_suite({ name: '{db: client promise}', Transport: MongoDB,
+  construct: { db: mongodb.MongoClient.connect(dbUrl, { useNewUrlParser: true }) }});
+test_suite({ name: '{db: client promise, dbName: string}', Transport: MongoDB,
   construct: {
     dbName,
-    db: mongodb.MongoClient.connect(dbUrl, {useNewUrlParser: true})},
-  });
-test_suite({name: '{db: mongoose client}', Transport: MongoDB,
-    construct: {db: mongoose.connection}});
+    db: mongodb.MongoClient.connect(dbUrl, { useNewUrlParser: true }) }
+});
+test_suite({ name: '{db: mongoose client}', Transport: MongoDB,
+  construct: { db: mongoose.connection }});
